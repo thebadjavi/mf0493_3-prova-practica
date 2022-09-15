bb09c7306727cb708a0ef95edb5b48ea8f07f234
thebadjavi <jfernandezv@elpuig.xeill.net>
2022-09-15 16:39:58 +0200

    3

--- unit-testing/isogram.test.js
+++ unit-testing/isogram.test.js
@@ -1,3 +1,18 @@
 import {it, expect, describe} from 'vitest';
 import { isIsogram } from './isogram';
-
+/*
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+*/
+describe('isogram', () => {
+    it('should return true for Dermatoglyphics', () => {
+        expect(isIsogram('Dermatoglyphics')).toBe(true);
+    });
+    it('should return false for aba', () => {
+        expect(isIsogram('aba')).toBe(false);
+    });
+    it('should return false for moOse', () => {
+        expect(isIsogram('moOse')).toBe(false);
+    });
+});
